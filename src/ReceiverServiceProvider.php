75117c1c3c2dc67fc27f387e847847b256ad0811
Adam Campbell <adam@hotmeteor.com>
2022-08-20 08:30:54 -0500

    Update ReceiverServiceProvider.php

--- src/ReceiverServiceProvider.php
+++ src/ReceiverServiceProvider.php
@@ -2,15 +2,31 @@
 
 namespace Receiver;
 
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
+use Receiver\Contracts\Factory;
 
-class ReceiverServiceProvider extends ServiceProvider
+class ReceiverServiceProvider extends ServiceProvider implements DeferrableProvider
 {
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
     public function register()
     {
+        $this->app->singleton(Factory::class, function ($app) {
+            return new ReceiverManager($app);
+        });
     }
 
-    public function boot()
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
     {
+        return [Factory::class];
     }
 }
