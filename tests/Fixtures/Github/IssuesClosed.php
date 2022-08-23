b84d57383fff9dbfdb20caf8ca303e48c76c2c57
Adam Campbell <adam@hotmeteor.com>
2022-08-23 09:08:37 -0500

    Update IssuesClosed.php

--- tests/Fixtures/Github/IssuesClosed.php
+++ tests/Fixtures/Github/IssuesClosed.php
@@ -2,11 +2,21 @@
 
 namespace Receiver\Tests\Fixtures\Github;
 
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 use Receiver\Providers\Webhook;
 
-class IssuesClosed
+class IssuesClosed implements ShouldQueue
 {
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
     public function __construct(public string $name, public array $data, public Webhook $webhook)
     {
     }
