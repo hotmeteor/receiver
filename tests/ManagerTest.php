0fe2f44bf80a19beb5a40dac704acfa1b548bc45
Jan Kremlacek <36951+jankremlacek@users.noreply.github.com>
2022-08-24 12:05:59 +0200

    Fix invalid method name

--- tests/ManagerTest.php
+++ tests/ManagerTest.php
@@ -4,6 +4,7 @@ namespace Receiver\Tests;
 
 use Receiver\Contracts\Factory;
 use Receiver\Providers\GithubProvider;
+use Receiver\Providers\PostmarkProvider;
 
 class ManagerTest extends TestCase
 {
@@ -14,5 +15,14 @@ class ManagerTest extends TestCase
         $provider = $factory->driver('github');
 
         $this->assertInstanceOf(GithubProvider::class, $provider);
+    }
+
+    public function test_it_can_instantiate_the_postmark_driver()
+    {
+        $factory = $this->app->make(Factory::class);
+
+        $provider = $factory->driver('postmark');
+
+        $this->assertInstanceOf(PostmarkProvider::class, $provider);
     }
 }
